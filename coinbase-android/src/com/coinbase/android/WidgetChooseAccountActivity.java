bd952d6c52832064dc7288b697613f572682fd2f
Isaac Waller <isaac@siriusapplications.com>
2013-03-13 17:55:49 -0700

    Fix 2 crashes on the Galaxy S II

--- coinbase-android/src/com/coinbase/android/WidgetChooseAccountActivity.java
+++ coinbase-android/src/com/coinbase/android/WidgetChooseAccountActivity.java
@@ -1,14 +1,15 @@
 package com.coinbase.android;
 
-import com.coinbase.api.LoginManager;
-
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
 
-public class WidgetChooseAccountActivity extends CoinbaseActivity implements AccountsFragment.ParentActivity {
+import com.coinbase.api.LoginManager;
+
+public class WidgetChooseAccountActivity extends FragmentActivity implements AccountsFragment.ParentActivity {
 
   @Override
   protected void onCreate(Bundle arg0) {
