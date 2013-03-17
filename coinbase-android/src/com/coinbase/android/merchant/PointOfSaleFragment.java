ffee831ac8696dba3a7d15d3b84cd45c6d2dff4d
Isaac Waller <isaac@siriusapplications.com>
2013-03-17 15:31:57 -0700

    Added layout for point of sale

--- coinbase-android/src/com/coinbase/android/merchant/PointOfSaleFragment.java
+++ coinbase-android/src/com/coinbase/android/merchant/PointOfSaleFragment.java
@@ -20,7 +20,7 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
     // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_transfer, container, false);
+    View view = inflater.inflate(R.layout.fragment_point_of_sale, container, false);
 
     return view;
   }
