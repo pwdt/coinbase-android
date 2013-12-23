9599780debecd1abe9df5ff281f679296e35e5e1
Isaac Waller <isaac@siriusapplications.com>
2013-12-22 20:01:04 -0800

    POS progress

--- coinbase-android/src/com/coinbase/android/merchant/PointOfSaleFragment.java
+++ coinbase-android/src/com/coinbase/android/merchant/PointOfSaleFragment.java
@@ -1,14 +1,20 @@
 package com.coinbase.android.merchant;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +29,7 @@ import android.widget.ViewFlipper;
 
 import com.coinbase.android.CoinbaseFragment;
 import com.coinbase.android.Constants;
+import com.coinbase.android.FontManager;
 import com.coinbase.android.MainActivity;
 import com.coinbase.android.R;
 import com.coinbase.android.Utils;
@@ -35,11 +42,15 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
 
@@ -53,7 +64,7 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
       String title = strings[2];
 
       if (title == null || title.trim().equals("")) {
-        title = "Merchant did not enter a title for this transaction (Android point of sale)";
+        title = "Android point of sale transaction";
       }
 
       List<BasicNameValuePair> params = new ArrayList<BasicNameValuePair>();
@@ -87,24 +98,24 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
 
       if (o == null) {
 
-        showResult(false, R.string.pos_result_failure_creation);
+        showResult(null, R.string.pos_result_failure_creation, null);
       } else if (o instanceof Exception) {
 
-        showResult(false, mParent.getString(R.string.pos_result_failure_creation_exception, ((Exception) o).getMessage()));
+        showResult(null, mParent.getString(R.string.pos_result_failure_creation_exception, ((Exception) o).getMessage()), null);
       } else {
 
         JSONObject result = (JSONObject) o;
 
         if (!result.optBoolean("success")) {
 
-          showResult(false, mParent.getString(R.string.pos_result_failure_creation_exception, result.toString()));
+          showResult(null, mParent.getString(R.string.pos_result_failure_creation_exception, result.toString()), null);
         } else {
 
           try {
-            populateAccept(result.getJSONObject("order"));
+            startAccepting(result.getJSONObject("order"));
           } catch (JSONException e) {
 
-            showResult(false, mParent.getString(R.string.pos_result_failure_creation_exception, e.getMessage()));
+            showResult(null, mParent.getString(R.string.pos_result_failure_creation_exception, e.getMessage()), null);
           }
         }
       }
@@ -176,10 +187,79 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
     }
   }
 
+  private enum CheckStatusState {
+    SUCCESS,
+    FAILURE,
+    DONE;
+  }
+
+  private class CheckStatusTask extends TimerTask {
+
+    private Context mContext;
+    private TextView mStatus = null;
+    private Handler mHandler;
+    private String mOrderId;
+
+    private JSONObject mOrder = null;
+
+    public CheckStatusTask(Context context, TextView statusView, String orderId) {
+
+      mContext = context;
+      mStatus = statusView;
+      mHandler = new Handler();
+      mOrderId = orderId;
+    }
+
+    public void run() {
+
+      final CheckStatusState state = doCheck();
+      if(state == CheckStatusState.DONE) {
+
+        // TODO cancel timer
+        paymentAccepted(mOrder);
+      } else {
+
+        // Update status indicator
+        mHandler.post(new Runnable() {
+
+          public void run() {
+
+            mStatus.setText(state.toString());
+          }
+        });
+      }
+    }
+
+    private CheckStatusState doCheck() {
+      try {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        int activeAccount = prefs.getInt(Constants.KEY_ACTIVE_ACCOUNT, -1);
+        String currentUserId = prefs.getString(String.format(Constants.KEY_ACCOUNT_ID, activeAccount), null);
+
+        JSONObject response = RpcManager.getInstance().callGet(mContext, "orders/" + mOrderId);
+        if(response.optJSONObject("order") != null) {
+
+          mOrder = response.optJSONObject("order");
+          return CheckStatusState.DONE;
+        } else {
+          // Successful check, but the order isn't in yet.
+          return CheckStatusState.SUCCESS;
+        }
+
+      } catch (Exception e) {
+        // Check was a failure - make sure to alert the user.
+        e.printStackTrace();
+        return CheckStatusState.FAILURE;
+      }
+    }
+  }
+
   private static final int INDEX_MAIN = 0;
   private static final int INDEX_LOADING = 1;
   private static final int INDEX_ACCEPT = 2;
   private static final int INDEX_RESULT = 3;
+  private static final int CHECK_PERIOD = 2000;
 
   private MainActivity mParent;
 
@@ -191,11 +271,17 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
 
   private ImageView mAcceptQr;
   private TextView mAcceptDesc;
+  private TextView mAcceptStatus;
   private Button mAcceptCancel;
+
+  private TextView mResultStatus, mResultMessage;
+  private Button mResultOK;
 
   private View[] mHeaders;
   private TextView[] mHeaderTitles;
   private ImageView[] mHeaderLogos;
+
+  private Timer mCheckStatusTimer = null;
 
   @Override
   public void onSwitchedTo() {
@@ -222,6 +308,15 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
     mAcceptCancel = (Button) view.findViewById(R.id.pos_accept_cancel);
     mAcceptQr = (ImageView) view.findViewById(R.id.pos_accept_qr);
     mAcceptDesc = (TextView) view.findViewById(R.id.pos_accept_desc);
+    mAcceptStatus = (TextView) view.findViewById(R.id.pos_accept_waiting);
+
+    mResultStatus = (TextView) view.findViewById(R.id.pos_result_status);
+    mResultMessage = (TextView) view.findViewById(R.id.pos_result_msg);
+    mResultOK = (Button) view.findViewById(R.id.pos_result_ok);
+
+    mSubmit.setTypeface(FontManager.getFont(mParent, "Roboto-Light"));
+    mAcceptCancel.setTypeface(FontManager.getFont(mParent, "Roboto-Light"));
+    mResultOK.setTypeface(FontManager.getFont(mParent, "Roboto-Light"));
 
     mSubmit.setOnClickListener(new View.OnClickListener() {
 
@@ -231,6 +326,18 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
         mFlipper.setDisplayedChild(INDEX_LOADING);
         new CreateButtonTask().execute(mAmount.getText().toString(),
                 (String) mCurrency.getSelectedItem(), mNotes.getText().toString());
+      }
+    });
+    mAcceptCancel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        stopAccepting();
+      }
+    });
+    mResultOK.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mFlipper.setDisplayedChild(INDEX_MAIN);
       }
     });
     
@@ -249,7 +356,7 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
       }});
 
     // Headers
-    int[] headers = { R.id.pos_accept_header, R.id.pos_main_header };
+    int[] headers = { R.id.pos_accept_header, R.id.pos_main_header, R.id.pos_result_header };
 
     mHeaders = new View[headers.length];
     mHeaderTitles = new TextView[headers.length];
@@ -309,11 +416,12 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
     mParent = (MainActivity) activity;
   }
 
-  private void populateAccept(JSONObject order) throws JSONException {
+  private void startAccepting(JSONObject order) throws JSONException {
 
     String receiveAddress = order.getString("receive_address");
-    String amount = Double.toString(order.getJSONObject("total_btc").getDouble("cents") / 100d);
+    String amount = moneyToValue(order.getJSONObject("total_btc")).toPlainString();
     String bitcoinUri = String.format("bitcoin:%1$s?amount=%2$s", receiveAddress, amount);
+    String orderId = order.getString("id");
 
     Bitmap bitmap;
     try {
@@ -323,18 +431,83 @@ public class PointOfSaleFragment extends Fragment implements CoinbaseFragment {
       bitmap = null;
     }
     mAcceptQr.setImageBitmap(bitmap);
+    mAcceptDesc.setText("asdf");
+
+    mCheckStatusTimer = new Timer();
+    mCheckStatusTimer.schedule(new CheckStatusTask(mParent, mAcceptStatus, orderId), CHECK_PERIOD, CHECK_PERIOD);
 
     mFlipper.setDisplayedChild(INDEX_ACCEPT);
   }
 
-  private void showResult(boolean success, int message) {
-    showResult(success, mParent.getString(message));
+  private void stopAccepting() {
+
+    mCheckStatusTimer.cancel();
+    mCheckStatusTimer = null;
+    showResult("cancelled", null, null);
   }
 
-  private void showResult(boolean success, String message) {
+  private void paymentAccepted(final JSONObject order) {
 
-    // TODO
+    mCheckStatusTimer.cancel();
+    mCheckStatusTimer = null;
+    ((Vibrator) mParent.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(500 );
+
+    mParent.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        showResult(order.optString("status"), null, order);
+      }
+    });
+  }
+
+  private void showResult(String status, int message, JSONObject order) {
+    showResult(status, mParent.getString(message), order);
+  }
+
+  private void showResult(String status, String message, JSONObject order) {
+
+    if (status == null) {
+      status = "ERROR";
+    } else {
+      status = status.toUpperCase(Locale.CANADA);
+    }
+
+    int color;
+    if ("COMPLETED".equals(status)) {
+
+      String amount = Utils.formatCurrencyAmount(moneyToValue(order.optJSONObject("total_native")), false, Utils.CurrencyType.TRADITIONAL);
+      String currency = order.optJSONObject("total_native").optString("currency_iso").toUpperCase(Locale.CANADA);
+      message = getString(R.string.pos_result_completed, amount, currency);
+      color = R.color.pos_result_completed;
+    } else if ("MISPAID".equals(status)) {
+      message = getString(R.string.pos_result_mispaid);
+      color = R.color.pos_result_mispaid;
+    } else {
+      color = R.color.pos_result_error;
+    }
+
+    float radius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3, getResources().getDisplayMetrics());
+    ShapeDrawable background = new ShapeDrawable(new RoundRectShape(
+            new float[] { radius, radius, radius, radius, radius, radius, radius, radius }, null, null));
+    background.getPaint().setColor(getResources().getColor(color));
+    mResultStatus.setBackgroundDrawable(background);
+
+    mResultStatus.setText(status);
+    mResultMessage.setText(message);
     mFlipper.setDisplayedChild(INDEX_RESULT);
+  }
+
+  private BigDecimal moneyToValue(JSONObject money) {
+
+    String currency = money.optString("currency_iso");
+    BigDecimal cents = new BigDecimal(money.optString("cents"));
+    BigDecimal result;
+    if ("BTC".equals(currency)) {
+      result = cents.multiply(new BigDecimal(0.00000001), MathContext.DECIMAL128);
+    } else {
+      result = cents.multiply(new BigDecimal(0.01), MathContext.DECIMAL128);
+    }
+    return result.setScale(10, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
   }
 
   @Override
