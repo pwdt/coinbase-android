7d9368e4df394578f0a77be811a3bdefed51d7a0
Alex Ianus <aianus@coinbase.com>
2014-07-30 14:50:01 +0700

    Fix BitcoinUri class

--- coinbase-android/src/androidTestConsumer/java/com/coinbase/android/test/BitcoinUriTest.java
+++ coinbase-android/src/androidTestConsumer/java/com/coinbase/android/test/BitcoinUriTest.java
@@ -1,6 +1,6 @@
 package com.coinbase.android.test;
 
-import com.coinbase.android.BitcoinUri;
+import com.coinbase.android.util.BitcoinUri;
 
 import junit.framework.TestCase;
 
@@ -12,7 +12,7 @@ public class BitcoinUriTest extends TestCase {
     String uriString = "bitcoin:1NLsANR9nhi91CrekNPuvNoUy549cuEUZU?amount=2&message=derp&label=thelabel";
     BitcoinUri bitcoinUri = BitcoinUri.parse(uriString);
     assertEquals("1NLsANR9nhi91CrekNPuvNoUy549cuEUZU", bitcoinUri.getAddress());
-    assertEquals(new BigDecimal("2.0"), bitcoinUri.getAmount());
+    assertEquals(new BigDecimal("2.00000000"), bitcoinUri.getAmount());
     assertEquals("derp", bitcoinUri.getMessage());
     assertEquals("thelabel", bitcoinUri.getLabel());
   }
@@ -20,10 +20,10 @@ public class BitcoinUriTest extends TestCase {
   public void testGenerating() throws Exception {
     BitcoinUri uri = new BitcoinUri();
     uri.setAddress("1NLsANR9nhi91CrekNPuvNoUy549cuEUZU");
-    uri.setAmount(new BigDecimal("2.0"));
+    uri.setAmount(new BigDecimal("2.00000000"));
     uri.setMessage("derp");
     uri.setLabel("theLabel");
-    assertEquals("bitcoin:1NLsANR9nhi91CrekNPuvNoUy549cuEUZU?amount=2.00000000&message=derp&label=thelabel", uri.toUri());
+    assertEquals("bitcoin:1NLsANR9nhi91CrekNPuvNoUy549cuEUZU?amount=2.00000000&message=derp&label=theLabel", uri.toString());
   }
 
 }
